5e63ed0b1ed017644f4498af19969ec8000f0dc2
Heather Haymond <Hheatherjean11@gmail.com>
2023-04-20 21:56:05 -0500

    comments and components

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import  Button  from 'react-bootstrap/Button';
 import  { Card }  from 'react-bootstrap';
 
+// Book component
 export function Book() {
+    // state variables to store book data and input values
     const [books, setBooks] = useState([]);
     // const [cart, setCart] = useState([]);
     const [bookName, setBookName] = useState ('')
@@ -16,12 +18,13 @@ export function Book() {
       getBooks()
     }, []);
 
+    // function to fetch books from the API 
   const getBooks = async() =>{
    await fetch('https://642725c4161067a83bf6687e.mockapi.io/Books')
         .then(response => response.json())
         .then(data => { 
-            let sortedData=data.reverse()
-            setBooks(sortedData)
+            let sortedData=data.reverse() // reverse the data array to reverse book order
+            setBooks(sortedData) // set the book data to the state
         })
         .catch(error => alert(error));
   }
@@ -29,7 +32,6 @@ export function Book() {
     // const handleAddToCart = book => {
     //   setCart(prevCart => [...prevCart, book]);
     // };
-    // function to add a review for a book
    
   // function to delete 
   const handleDelete = async (id) => {
@@ -40,9 +42,9 @@ export function Book() {
     "Content-Type": "application/json",
 },
 })
-    getBooks()
+    getBooks() // set the book data to the state
   };
-  //function that adds btn
+  // function to add a new book to the API
   const handleSubmit = async e => {
     console.log("posting a new book...")
     e.preventDefault();
@@ -62,11 +64,13 @@ body: JSON.stringify(newBooks),
     // const createdBook = await response.json()
     //spread operater to st values of books
     // setBooks(prevBooks => [...prevBooks, createdBook]);
+
+    // reset input value for:
     setBookName('');
     setBookAuthor('');
     setBookPublisher('');
     setBookCoverImage('');
-    getBooks('')
+    getBooks('') // call getBooks function to update the state
   };
    // function to update a book in the API
   const handleUpdate = async (id, name, URL) => {
@@ -77,55 +81,55 @@ body: JSON.stringify(newBooks),
         Publisher: bookPublisher,
         CoverImage: URL,
         };
-    const response = await fetch(`https://642725c4161067a83bf6687e.mockapi.io/Books/${id}`, {
-      method: 'PUT',
-      headers: {
+    const response = await fetch(`https://642725c4161067a83bf6687e.mockapi.io/Books/${id}`, { //sends a PUT request to the API to update the book data
+      method: 'PUT', //specifies that this is a PUT request
+      headers: { //sets the headers for the request as well as data sent to JSON 
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(newBooks),
+      body: JSON.stringify(newBooks), //converts the new book data into a JSON string and sets it as the body of the request
     });
-getBooks()
+getBooks() //calls the getBooks function to update new data
   };
 
     return (
         <div>
         <h1>Book</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}> //sets up a form with an onSubmit handler to add a new book
            <label>
              Add a new Book:
-             <input type="text" value={bookName} onChange={e => setBookName(e.target.value)} />
+             <input type="text" value={bookName} onChange={e => setBookName(e.target.value)} />  //sets up an input field for the book name and updates the bookName state when the input changes
              </label>
              <label>
              Author:
-             <input type="text" value={bookAuthor} onChange={e => setBookAuthor(e.target.value)} />
+             <input type="text" value={bookAuthor} onChange={e => setBookAuthor(e.target.value)} /> //sets up an input field for the book author and updates the bookAuthor state when the input changes
              </label>
              <label>
              Publisher:
-             <input type="text" value={bookPublisher} onChange={e => setBookPublisher(e.target.value)} />
+             <input type="text" value={bookPublisher} onChange={e => setBookPublisher(e.target.value)} /> //sets up an input field for the book cover image URL and updates the bookCoverImage state when the input changes
              </label>
              <label>
              Cover Image URL:
-             <input type="text" value={bookCoverImage} onChange={e => setBookCoverImage(e.target.value)} />
+             <input type="text" value={bookCoverImage} onChange={e => setBookCoverImage(e.target.value)} /> sets up an input field for the book cover image URL and updates the bookCoverImage state when the input changes
              </label>
-             <Button type="submit" variant="primary">add</Button>
+             <Button type="submit" variant="primary">add</Button> {/*a button to add the new book*/}
             </form>
         <ul>
-          {books.reverse().map((book) => ( //iterates
+          {books.reverse().map((book) => ( //iterates over the list of books in reverse order and generates a card for each one
             <li key={book.id}>
                 <Card style={{ width: '18rem' }}>
-            <Card.Img src={book.CoverImage} alt={book.Title} />
+            <Card.Img src={book.CoverImage} alt={book.Title} /> //displays the book cover image
             <h2>{book.Title}</h2>
             <p>by {book.Author}</p>
             <p>Publisher: {book.Publisher}</p> 
-            <Button variant="danger" onClick={() => handleDelete(book.id)}>Delete</Button>  
-            <form onSubmit={handleSubmit}>
+            <Button variant="danger" onClick={() => handleDelete(book.id)}>Delete</Button>  {/*a button to delete the current book*/}
+            <form onSubmit={handleSubmit}>//form with onSubmit handler that updates book data
            <label>
              Update a Book:
-             <input type="text" value={updatedBookName} onChange={e => setUpdatedBookName(e.target.value)} />
+             <input type="text" value={updatedBookName} onChange={e => setUpdatedBookName(e.target.value)} /> // input field for updated book name and updates the updatedBookName state when the input changes
              cover Image URL:
              <input type="text" value={updatedBookCoverImage} onChange={e => setUpdatedBookCoverImage(e.target.value)} />
              </label>
-             <Button variant="warning" onClick={() => handleUpdate(book.id,updatedBookName,updatedBookCoverImage)}>Edit</Button>
+             <Button variant="warning" onClick={() => handleUpdate(book.id,updatedBookName,updatedBookCoverImage)}>Edit</Button> {/*a button to update the current book*/}
              </form>
              </Card>
           </li>
--- src/components/Magazines.js
+++ src/components/Magazines.js
@@ -1,32 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import { Card } from 'react-bootstrap';
+
 import { useParams } from 'react-router-dom';
+
+//magazine component
 export function Magazines() {
+    // state variables to store data and input values
     const [magazines, setMagazines] = useState([]);
     // const [cart, setCart] = useState([]);
     const [magazineName, setMagazineName] = useState ('')
-    
-    // useEffect to fetch books from an external API and set them to the state
+    const [MagazineAuthor, setMagazineAuthor] = useState ('')
+    const [magazinePublisher, setMagazinePublisher] = useState ('')
+    const [magazineCoverImage, setMagazineCoverImage] = useState ('')
+    const [updatedMagazineName, setUpdatedMagazineName] = useState ('')
+    const [updatedMagazineCoverImage, setUpdatedMagazineCoverImage] = useState ('')
+    // useEffect to fetch magazines from an external API and set them to the state
     useEffect(() => {
-      fetch('https://642725c4161067a83bf6687e.mockapi.io/Magazines')
-        .then(response => response.json())
-        .then(data => setMagazines(data))
-        .catch(error => alert(error));
+        getMagazines()
     }, []);
+    //function to fetch from the API
+    const getMagazines = async() =>{
+    await fetch('https://642725c4161067a83bf6687e.mockapi.io/Magazines')
+        .then(response => response.json())
+        .then(data => {
+            let sortedData=data.reverse() //reverse the data array
+            setMagazines(sortedData) //set data to the state
+        })
+        .catch(error => alert(error));
+    }
   
     // function to add a book to the cart
     // const handleAddToCart = magazine => {
     //   setCart(prevCart => [...prevCart, magazine]);
     // };
   // function to delete 
-  const handleDelete = id => {
-    fetch ("https://642725c4161067a83bf6687e.mockapi.io/Magazines/"+id,{method:"DELETE"})
-    setMagazines(prevMagazines => prevMagazines.filter(p => p.id !== id));
+  const handleDelete = async (id) => {
+    console.log("deletingMagazine...", id)
+   const response = await fetch(`https://642725c4161067a83bf6687e.mockapi.io/Magazines/${id}`,{
+    method:"DELETE",
+    headers: {
+    "content-Type": "application/json"
+   },
+})
+getMagazines()  // set book to state
+    // setMagazines(prevMagazines => prevMagazines.filter(p => p.id !== id));
   };
-  //function that adds btn
+  //function that adds magazine to the API
   const handleSubmit = async e => {
+    console.log ("posting a new magazine...")
     e.preventDefault();
     const newMagazines = {
-        title: magazineName
+        Title: magazineName,
+        Author: MagazineAuthor,
+        Publisher: magazinePublisher,
+        CoverImage: magazineCoverImage,
 
     };
     const response = await fetch ("https://642725c4161067a83bf6687e.mockapi.io/Magazines",{
@@ -36,11 +64,42 @@ export function Magazines() {
       },
       body: JSON.stringify(newMagazines),
     })
-    const createdMagazine = await response.json()
+
+    
+    // const createdBook = await response.json()
     //spread operater to st values of books
-    setMagazines(prevMagazines => [...prevMagazines, createdMagazine]);
+    // setBooks(prevBooks => [...prevBooks, createdBook]);
+    
+    // reset input value for:
     setMagazineName('');
-  };
+    setMagazineAuthor('');
+    setMagazinePublisher('');
+    setMagazineCoverImage('');
+    getMagazines('') // call getMagazines function to update the state
+    };
+    // function to update a book in the API
+    const handleUpdate = async (id, name, URL) => {
+    console.log("updated magazine...", id, name, URL)
+    const newMagazines = {
+    Title: name,
+    Author: MagazineAuthor,
+    Publisher: magazinePublisher,
+    CoverImage: URL,
+    };
+    const response = await fetch(`https://642725c4161067a83bf6687e.mockapi.io/Magazines/${id}`, { //sends a PUT request to the API to update the magazine data
+    method: 'PUT', //specifies that this is a PUT request
+    headers: { //sets the headers for the request as well as data sent to JSON 
+    'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newMagazines), //converts the new book data into a JSON string and sets it as the body of the request
+    });
+    getMagazines() //calls the getBooks function to update new data
+    };
+//     const createdMagazine = await response.json()
+    
+//     setMagazines(prevMagazines => [...prevMagazines, createdMagazine]);
+//     setMagazineName('');
+//   };
 
     // function to remove a book from the cart
     // const handleRemoveFromCart = magazine => {
@@ -56,16 +115,31 @@ export function Magazines() {
             Add a new Magazine:
             <input type="text" value={magazineName} onChange={e => setMagazineName(e.target.value)} />
           </label>
-          <button type="submit">Add</button>
+          <label>
+Cover Image URL:
+            <input type="text" value={magazineCoverImage} onChange={e => setMagazineCoverImage(e.target.value)} /> sets up an input field for the magazine image URL and updates the bookCoverImage state when the input changes
+          </label>
+          <Button variant="warning" onClick={() => handleUpdate(magazines.id,updatedMagazineName,updatedMagazineCoverImage)}>Edit</Button> 
         </form>
         <ul>
-          {magazines.map((magazine) => ( //iterates
+          {magazines.reverse().map((magazine) => ( //iterates
             <li key={magazine.id}>
-            <img src={magazine.coverImage} alt={magazine.title} />
-            <h2>{magazine.title}</h2>
-            <p>by {magazine.author}</p>
-            <p>Publisher: {magazine.publisher}</p>
-            <button onClick={() => handleDelete(magazine.id)}>Delete</button>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img src={magazine.CoverImage} alt={magazine.Title} /> //displays the book cover image
+            <h2>{magazine.Title}</h2>
+            <p>by {magazine.Author}</p>
+            <p>Publisher: {magazine.Publisher}</p> 
+            <Button variant="danger" onClick={() => handleDelete(magazine.id)}>Delete</Button> {/*a button to delete the current book*/}
+            <form onSubmit={handleSubmit}>//form with onSubmit handler that updates book data
+            <label>
+                Updade a Magazine:
+                <input type="text" value={updatedMagazineName} onChange={e => setUpdatedMagazineName(e.target.value)} />
+                cover Image URL:
+                <input type="text" value={updatedMagazineCoverImage} onChange={e => setUpdatedMagazineCoverImage(e.target.value)} />
+                </label>
+                <Button variant="warning" onClick={() => handleUpdate(magazine.id,updatedMagazineName,updatedMagazineCoverImage)}>Edit</Button> {/*a button to update the current book*/}
+                </form>
+                </Card>
           </li>
           ))}
         </ul>
