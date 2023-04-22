641097b7047f1bc2f9ca59d0e676d8c6c76fcd1f
Heather Haymond <Hheatherjean11@gmail.com>
2023-04-22 13:53:39 -0500

    adeded components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TopBar from './components/TopBar'
 import { Route, Routes, Link } from 'react-router-dom'
 import  Home  from "./components/Home"
-//import { NotFound } from './components/NotFound'
-//import { BookLayout } from './BookLayout'
-// import {  } from './BookRoutes'
 //import { NewBook } from './components/NewBook'
 import { Magazines } from './components/Magazines'
 // import SearchResultsPage from './components/SearchResultsPage'
-import BookDetailPage from './components/BookDetailPage'
 import BookCard from './components/BookCard'
 // import { Cart } from './components/Cart'
 import { Book } from './components/Book'
-import { Review } from './components/Review'
-import { ReviewForm } from './components/ReviewForm'
-import { ReviewList } from './components/ReviewList'
+import SearchResults from './components/SearchResults';
+
 export default function App() {
-// const [bookList] = useState(TEST_BOOKS)
   return (
 
     <>
@@ -30,7 +24,7 @@ export default function App() {
 
       <ul>
         <li>
-          <Link to="/Home">Home</Link>
+          <Link to="/">Home</Link>
           </li>
           <li>
           <Link to="/Books">Books</Link>
@@ -41,15 +35,7 @@ export default function App() {
           {/* <li>
           <Link to="/Cart">Cart </Link>
           </li>
-          <li>
-          <Link to="/Review">Reviews </Link>
-          </li>
-          <li>
-          <Link to="/ReviewForm">Add Review</Link>
-          </li>
-          <li>
-          <Link to="/ReviewList">Review List</Link>
-          </li> */}
+         */}
           
       </ul>
     </nav>
@@ -57,16 +43,10 @@ export default function App() {
     <TopBar /> 
     {/* nav bar */}
       <Routes>
-        {/* <Route path="/" element={ <Home bookList={bookList} />} /> */}
+        <Route path="/" element={ <Home />} />
         <Route path="/Books/*" element={<Book />}  />
         <Route path="/Magazines/*" element={<Magazines />}  />
-        {/* <Route path="/Cart/*" element={<Cart />} />
-        <Route path="/Review/*" element={<Cart />} />
-        <Route path="/ReviewForm/*" element={<Cart />} />
-        <Route path="/ReviewList/*" element={<Cart />} /> */}
-        {/* <Route path="/Search" element={<SearchResultsPage bookList={bookList} />} />
-        <Route path="/search/:searchQuery" element={ <SearchResultsPage bookList={bookList}/> }/>
-        <Route path="/book/:bookId" element={ <BookDetailsPage bookList={bookList}/> }/> */}
+        <Route path="/search/:searchQuery" element={<SearchResults />} />
        </Routes>
 
     </>
